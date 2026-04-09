af5bff8d9a9f5bb4d81d70d503aa43200ab03fe4
Annh Hào <thanhprokume1@gmail.com>
2026-04-09 12:10:53 +0700

    server

---
+++ server/utils/EmailUtil.js
@@ -0,0 +1,43 @@
+// CLI: npm install nodemailer --save
+
+const nodemailer = require('nodemailer');
+const MyConstants = require('./MyConstants');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: MyConstants.EMAIL_USER,
+    pass: MyConstants.EMAIL_PASS,
+  },
+});
+
+const EmailUtil = {
+  send(email, id, token) {
+    const text =
+      'Thanks for signing up, please input these informations to activate your account:\n' +
+      '\t.id: ' +
+      id +
+      '\n' +
+      '\t.token: ' +
+      token;
+
+    return new Promise((resolve, reject) => {
+      const mailOptions = {
+        from: MyConstants.EMAIL_USER,
+        to: email,
+        subject: 'Signup | Verification',
+        text: text,
+      };
+
+      transporter.sendMail(mailOptions, (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(true);
+      });
+    });
+  },
+};
+
+module.exports = EmailUtil;
